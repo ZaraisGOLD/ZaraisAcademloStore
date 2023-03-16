74bac7b97f5f8162e9f874a76d98286e9a94fbc1
ZaraisGOLD <zaraisgold@gmail.com>
2023-03-16 17:16:55 -0400

    c23

--- assets/js/main.js
+++ assets/js/main.js
@@ -250,7 +250,7 @@ function handleTotal(dataBase) {
         printProductsCart(dataBase);
         printProducts(dataBase);
         handlePrintAmountProducts(dataBase);
-        handleFilters();
+        location.reload();
     });
 }
 
